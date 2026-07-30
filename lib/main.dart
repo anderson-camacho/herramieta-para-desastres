e157b3add48433669df95ddb29f050bf37c229e7
Anderson Camacho <54694564+anderson-camacho@users.noreply.github.com>
2026-07-29 23:01:48 -0500

    Use renamed application entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'package:signal_scope/src/app/signal_scope_app.dart';
+import 'package:signal_scope/src/app/herramienta_desastres_app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const SignalScopeApp());
+  runApp(const HerramientaDesastresApp());
 }
-
